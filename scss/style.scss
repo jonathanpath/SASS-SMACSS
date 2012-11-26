8c91f88f7bb2f6fcd815ffb6de08a0fb5ce95409
Jonathan Path <jonathan.path@gmail.com>
2012-11-26 12:20:42 +0100

    First import

---
+++ scss/style.scss
@@ -0,0 +1,32 @@
+/* 
+ * SMACSS + SCSS starter v1.0
+ * Inspired by SMACSS http://smacss.com
+ * Author: @jonathanpath
+ *
+ * https://github.com/jonathanpath/SMACSS-SASS-starter
+ * 
+ */
+
+/* Base */
+@import "base/mixins";
+@import "base/normalize";
+@import "base/site-settings";
+@import "base/site-settings-load";
+@import "base/helpers";
+
+/* Layout */
+@import "layout/header";
+@import "layout/footer";
+@import "layout/nav";
+@import "layout/wrapper";
+@import "layout/cols";
+
+/* Modules */
+@import "modules/btn";
+@import "modules/slider";
+@import "modules/icons";
+@import "modules/animations";
+@import "modules/tagcloud";
+
+/* Non-Modular */
+@import "non-modular/organisation";
