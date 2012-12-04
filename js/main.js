65821c097fcd4040cdd39b3e730fb1e46efa4642
Jonathan Path <jonathan.path@gmail.com>
2012-12-04 15:53:38 +0100

    Add titles File / New JS functions

--- js/main.js
+++ js/main.js
@@ -17,4 +17,41 @@ $(document).ready(function(){
 		event.stopPropagation();
 	});
 	
+	// placeholder for old browsers
+	if (!Modernizr.input.autofocus) {
+        $('input').each(function(){
+			if ($(this).attr('autofocus'))
+				$(this).focus();
+		});
+    }
+	if (!Modernizr.input.placeholder) {
+        $('input').each(function(){
+			if ($(this).attr('placeholder')) {
+				var placeholder_text = $(this).attr('placeholder');
+				$(this).attr('value', placeholder_text).addClass('nc');
+			}
+		});
+		
+		// for all fields with uncleared initial value, on focus
+		$('.nc').focus(function() {
+
+			// if it is uncleared
+			if ($(this).hasClass('nc')) {
+
+				// remeber the initial value
+				$textbox = $(this).val();
+
+				// and remove it
+				$(this).removeClass('nc').val('');
+			}
+		}).focusout(function() { // on focus out
+
+			// if there is no new value posted by user
+			if ($(this).val() == '')
+
+				// mark as uncleared and show initial value
+				$(this).addClass('nc').val($textbox);
+		});
+    }
+	
 });
