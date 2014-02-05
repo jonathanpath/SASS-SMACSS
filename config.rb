6ec761abaf23fe9875df058d6b8b2f79be160ae8
Jonathan Path <contact@jonathanpath.com>
2014-02-05 11:12:49 +0100

    Improvements

--- config.rb
+++ config.rb
@@ -1,7 +1,7 @@
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
-http_path = "/"
+http_path = "../"
 css_dir = "css"
 sass_dir = "scss"
 images_dir = "img"
